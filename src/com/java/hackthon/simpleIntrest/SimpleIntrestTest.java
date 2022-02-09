c70c86ab3617562578d454d26f6b0cabe5ac04b8
Dasari-Ravi <89849794+Dasari-Ravi@users.noreply.github.com>
2022-02-09 13:06:06 +0530

    Initial Commit
    Initial Commit

---
+++ src/com/java/hackthon/simpleIntrest/SimpleIntrestTest.java
@@ -0,0 +1,28 @@
+package com.java.hackthon.simpleIntrest;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SimpleIntrestTest {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+/// nbnbnb
+		String principalInput = scanner.next();
+		String interestInput = scanner.next();
+		String timeInput = scanner.next();
+		Pattern pattern = Pattern.compile("[0-9]");
+		Matcher principalMatcher = pattern.matcher(principalInput);
+		Matcher timeMatcher = pattern.matcher(timeInput);
+		boolean principalInputDecimal = principalInput.contains(".");
+		boolean timeInputDecimal = timeInput.contains(".");
+		if (principalMatcher.find() && timeMatcher.find() && !principalInputDecimal && !timeInputDecimal) {
+			int principal = Integer.valueOf(principalInput);
+			double interest = Double.valueOf(interestInput);
+			int time = Integer.valueOf(timeInput);
+			double total_ammount = (principal * interest * time) / 100;
+			System.out.println(total_ammount);
+		}
+		scanner.close();
+	}
+}
