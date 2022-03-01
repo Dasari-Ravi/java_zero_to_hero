a0a75bf6c3ef5cb965a1e095cbc4996cc4d5ee46
Dasari-Ravi <89849794+Dasari-Ravi@users.noreply.github.com>
2022-03-01 23:31:21 +0530

    Arrays

---
+++ src/com/java/core/arrays/ArrayConcepts.java
@@ -0,0 +1,40 @@
+package com.java.core.arrays;
+
+public class ArrayConcepts {
+	public static void main(String[] args) {
+		int[] myArrayValue = new int[25];
+		for (int i = 0; i < myArrayValue.length; i++) {
+			myArrayValue[i] = i * 10;
+
+		}
+		for (int i = 0; i <myArrayValue.length; i++) {
+			System.out.println("element " + i + " value is " + myArrayValue[i]);
+		}
+//		// {1,2,3,4,5,6,7,8,9,10};
+//		double[] myDoubleValue = new double[6];
+//
+////		myArrayValue[1]=20;
+////		myArrayValue[2]=30;
+////		myArrayValue[3]=40;
+////		myArrayValue[4]=50;
+////		myArrayValue[5]=60;
+////		myArrayValue[6]=70;
+////		myArrayValue[7]=80;
+////		myArrayValue[0]=90;
+////		myArrayValue[0]=10;
+//		System.out.println(myArrayValue[0]);
+//		System.out.println(myArrayValue[1]);
+//		System.out.println(myArrayValue[2]);
+//		System.out.println(myArrayValue[3]);
+//		System.out.println(myArrayValue[4]);
+//		System.out.println(myArrayValue[5]);
+//		System.out.println(myArrayValue[6]);
+//		System.out.println(myArrayValue[7]);
+//		System.out.println(myArrayValue[8]);
+//		System.out.println(myArrayValue[9]);
+////		System.out.println(myArrayValue[4]);
+////		System.out.println(myArrayValue[5]);
+
+	}
+
+}
